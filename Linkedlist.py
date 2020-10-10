b91462d004fd826b36c5ea427541f2a24194497f
nirum09 <nirupama.m09@gmail.com>
2020-10-10 18:40:04 +0530

    added description

--- Linkedlist.py
+++ Linkedlist.py
@@ -1,10 +1,11 @@
 class Node:
+    """Node class definition for Linkedlist"""
     def __init__(self,data):
         self.data=data
         self.next=None
 
 class LinkedList:
-
+    """Class definition for singly linkedlist with associated methods"""
     def __init__(self):
         self.head=None
 
