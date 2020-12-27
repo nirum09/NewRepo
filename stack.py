1746e671550210b63fd6fd88c047031d27729b3a
nirum09 <nirupama.m09@gmail.com>
2020-12-27 12:17:06 +0530

    stack class commit

--- stack.py
+++ stack.py
@@ -1,9 +1,33 @@
-stack=[]
+class Stack:
+    def __init__(self,length=10):
+        self.stack=[]
+        self.maxlength=length
+        print(f"Stack created of max size {self.maxlength}")
 
-stack.append('a')
-stack.append('b')
-stack.append('c')
+    def push(self,element):
+        if self.length()>=self.maxlength:
+            raise Exception("Overflow")
+        else:
+            self.stack.append(element)
+            return  self.stack
 
-print(stack)
-print(stack.pop())
-print(stack)
+    def Pop(self):
+        if self.length() > 0:
+            return self.stack.pop()
+        else:
+            raise Exception("Underflow")
+    def length(self):
+        return len(self.stack)
+
+
+if __name__=='__main__':
+    stack1 = Stack()
+    print(stack1.push(5))
+    print(stack1.push(8))
+    print(stack1.push(40))
+    print(stack1.push(34))
+    print(stack1.Pop())
+    print(stack1.length())
+
+
+
