44e79883a31d5765d683e7dbd3b9868325a9d209
nirum09 <nirupama.m09@gmail.com>
2021-01-23 12:23:26 +0530

    binary tree

---
+++ BST.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,d):
+        self.data=d
+        self.left=None
+        self.right=None
+
+def SortedArrtoBST(sorted_array):
+
+    if not sorted_array:
+        return None
+    mid=(len(sorted_array))/2
+    root=sorted_array[mid]
+
